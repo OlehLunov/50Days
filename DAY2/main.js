aec9ea0ebdbf015e44c01a4dd7ed8c84631e3099
Oleh Lunev <olegap2701@gmail.com>
2023-09-21 12:54:12 +0300

    1

---
+++ DAY2/main.js
@@ -0,0 +1,12 @@
+const progress = document.getElementById("step__progress");
+const circles = document.querySelectorAll(".circle");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+let active = 1;
+
+function move (){
+    circles.forEach((circle,index) =>{
+        
+    })
+}
